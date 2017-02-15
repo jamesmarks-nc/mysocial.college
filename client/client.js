bfd739432b8a2003fab2288b780d344e26b605c8
jamesgmarks <jamesgmarks@gmail.com>
2017-02-14 23:12:31 -0500

    c

--- client/client.js
+++ client/client.js
@@ -3,13 +3,18 @@
 // 2017-02-14
 // Client for mysocial.college
 
-fetch('/accounts')
-  .then((response) => {
-    return response.json();
-  })
+function getAccounts() {
+  return fetch('/accounts')
+    .then(response => response.json())
+    // .then((json) => {
+    //   console.log(json);
+    // })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+getAccounts()
   .then((json) => {
     console.log(json);
-  })
-  .catch((err) => {
-    console.error(err);
   });
